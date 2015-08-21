b04d15378482567eae9b365147f262f9be676748
Jin Shang <jin.shang@autodesk.com>
2015-08-20 22:41:24 -0700

    add uri anchor

--- spa3.2/js/spa.shell.js
+++ spa3.2/js/spa.shell.js
@@ -14,17 +14,13 @@
 
 spa.shell = (function() {
     var configMap = {
-            main_html: String() 
-            + "      <div class=\"spa-shell-head\">" 
-            + "            <div class=\"spa-shell-head-logo\"><\/div>" 
-            + "            <div class=\"spa-shell-head-acct\"><\/div>" 
-            + "            <div class=\"spa-shell-head-search\"><\/div>" 
-            + "        <\/div>" + "        <div class=\"spa-shell-main\">" 
-            + "            <div class=\"spa-shell-main-nav\"><\/div>" 
-            + "            <div class=\"spa-shell-main-content\"><\/div>" 
-            + "        <\/div>" + "        <div class=\"spa-shell-foot\"><\/div>" 
-            + "        <div class=\"spa-shell-chat\"><\/div>" 
-            + "        <div class=\"spa-shell-modal\"><\/div>",
+            anchor_schema_map: {
+                chat: {
+                    open: true,
+                    closed: true
+                }
+            },
+            main_html: String() + "      <div class=\"spa-shell-head\">" + "            <div class=\"spa-shell-head-logo\"><\/div>" + "            <div class=\"spa-shell-head-acct\"><\/div>" + "            <div class=\"spa-shell-head-search\"><\/div>" + "        <\/div>" + "        <div class=\"spa-shell-main\">" + "            <div class=\"spa-shell-main-nav\"><\/div>" + "            <div class=\"spa-shell-main-content\"><\/div>" + "        <\/div>" + "        <div class=\"spa-shell-foot\"><\/div>" + "        <div class=\"spa-shell-chat\"><\/div>" + "        <div class=\"spa-shell-modal\"><\/div>",
             chat_extend_time: 1000,
             chat_retract_time: 300,
             chat_extend_height: 450,
@@ -34,11 +30,50 @@ spa.shell = (function() {
         },
         stateMap = {
             $container: null,
+            anchor_map: {},
             is_chat_retracted: true
         }, //Place dynamic information shared across the moduel here
         jqueryMap = {}, //Cache jQuery collections here
-        setJqueryMap, initModule, toggleChat, onClickChat; //Declare all module scope variables
+        copyAnchorMap, changeAnchorPart, onHashchange, setJqueryMap, initModule, toggleChat, onClickChat; //Declare all module scope variables
 
+    copyAnchorMap = function() {
+        return $.extend(true, {}, stateMap.anchor_map);
+    };
+
+    changeAnchorPart = function(arg_map) {
+        var
+            anchor_map_revise = copyAnchorMap(),
+            bool_return = true,
+            key_name, key_name_dep;
+
+        KEYVAL:
+            for (key_name in arg_map) {
+                if (arg_map.hasOwnProperty(key_name)) {
+                    if (key_name.indexOf('_') === 0) {
+                        continue KEYVAL;
+                    }
+                    anchor_map_revise[key_name] = arg_map[key_name];
+                    key_name_dep = '_' + key_name;
+                    if (arg_map[key_name_dep]) {
+                        anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+                    } else {
+                        delete anchor_map_revise[key_name_dep];
+                        delete anchor_map_revise['_s' + key_name_dep];
+                    }
+                }
+            }
+
+        try {
+            $.uriAnchor.setAnchor(anchor_map_revise);
+        } catch (error) {
+            // replace URI with existing state
+            $.uriAnchor.setAnchor(stateMap.anchor_map, null, true);
+            bool_return = false;
+        }
+
+        return bool_return;
+
+    };
     //Cache jQuery collections. This function should be in almost every shell and feature module.
     //It can greatly reduce the number of jQuery document transversals and improve performance.
     setJqueryMap = function() {
@@ -49,40 +84,94 @@ spa.shell = (function() {
         };
     };
 
-    toggleChat = function (do_extend, callback) {
+    toggleChat = function(do_extend, callback) {
         var px_chat_ht = jqueryMap.$chat.height(),
-        is_Open = px_chat_ht === configMap.chat_extend_height,
-        is_Closed = px_chat_ht === configMap.chat_retract_height,
-        is_sliding = !is_Open && !is_Closed;
+            is_Open = px_chat_ht === configMap.chat_extend_height,
+            is_Closed = px_chat_ht === configMap.chat_retract_height,
+            is_sliding = !is_Open && !is_Closed;
 
-        if(is_sliding) { return false;}
-        if(do_extend) {
-            jqueryMap.$chat.animate(
-                {height: configMap.chat_extend_height},
+        if (is_sliding) {
+            return false;
+        }
+        if (do_extend) {
+            jqueryMap.$chat.animate({
+                    height: configMap.chat_extend_height
+                },
                 configMap.chat_extend_time,
-                function () {
+                function() {
                     jqueryMap.$chat.attr('title', configMap.chat_extend_title);
                     stateMap.is_chat_retracted = false;
-                    if(callback) {callback(jqueryMap.$chat);}
+                    if (callback) {
+                        callback(jqueryMap.$chat);
+                    }
                 });
             return true;
         }
 
-        jqueryMap.$chat.animate(
-            {height: configMap.chat_retract_height},
+        jqueryMap.$chat.animate({
+                height: configMap.chat_retract_height
+            },
             configMap.chat_retract_time,
             function() {
                 jqueryMap.$chat.attr('title', configMap.chat_retract_title);
                 stateMap.is_chat_retracted = true;
-                if(callback) {callback(jqueryMap.$chat);}
+                if (callback) {
+                    callback(jqueryMap.$chat);
+                }
             }
         );
 
         return true;
     };
 
-    onClickChat = function (event) {
-        toggleChat(stateMap.is_chat_retracted);
+    onHashchange = function(event) {
+        var
+            anchor_map_previous = copyAnchorMap(),
+            anchor_map_proposed,
+            _s_chat_previous, _s_chat_proposed,
+            s_chat_proposed;
+        // attempt to parse anchor
+        try {
+            anchor_map_proposed = $.uriAnchor.makeAnchorMap();
+        } catch (error) {
+            $.uriAnchor.setAnchor(anchor_map_previous, null, true);
+            return false;
+        }
+        stateMap.anchor_map = anchor_map_proposed;
+        // convenience vars
+        _s_chat_previous = anchor_map_previous._s_chat;
+        _s_chat_proposed = anchor_map_proposed._s_chat;
+        // Begin adjust chat component if changed
+        if (!anchor_map_previous || _s_chat_previous !== _s_chat_proposed) {
+            s_chat_proposed = anchor_map_proposed.chat;
+            switch (s_chat_proposed) {
+                case 'open':
+                    toggleChat(true);
+                    break;
+                case 'closed':
+                    toggleChat(false);
+                    break;
+                default:
+                    toggleChat(false);
+                    delete anchor_map_proposed.chat;
+                    $.uriAnchor.setAnchor(anchor_map_proposed, null, true);
+            }
+        }
+        // End adjust chat component if changed
+        return false;
+    };
+
+    onClickChat = function(event) {
+        // if (toggleChat(stateMap.is_chat_retracted)) {
+        //     $.uriAnchor.setAnchor({
+        //         chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+        //     });
+        // }
+
+        changeAnchorPart({
+            chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+        });
+
         return false;
     };
 
@@ -93,6 +182,16 @@ spa.shell = (function() {
         setJqueryMap();
         stateMap.is_chat_retracted = true;
         jqueryMap.$chat.attr('title', configMap.chat_retract_title).click(onClickChat);
+
+        $.uriAnchor.configModule({
+            schema_map: configMap.anchor_schema_map
+        });
+
+        $(window)
+            .bind('hashchange', onHashchange)
+            .trigger('hashchange');
+
+
 
         //test toggle
         // setTimeout(function() {toggleChat(true);}, 3000);
